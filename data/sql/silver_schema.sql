42b1fd9b22e0bde4e4e9438d4c2bc33a764e28c4
Jonathan Feng <124104961+jaytar0@users.noreply.github.com>
2026-04-18 17:35:58 -0700

    clean up tracked files

---
+++ data/sql/silver_schema.sql
@@ -0,0 +1,67 @@
+CREATE SCHEMA IF NOT EXISTS silver;
+
+-- HW5 ussage
+
+CREATE TABLE IF NOT EXISTS silver.hw5 (
+    symbol        TEXT        NOT NULL,
+    timestamp     TIMESTAMPTZ NOT NULL,
+    open          DOUBLE PRECISION,
+    high          DOUBLE PRECISION,
+    low           DOUBLE PRECISION,
+    close         DOUBLE PRECISION,
+    volume        DOUBLE PRECISION,
+    trade_count   DOUBLE PRECISION,
+    vwap          DOUBLE PRECISION,
+    processed_at   TIMESTAMPTZ NOT NULL DEFAULT now(),
+    PRIMARY KEY (symbol, timestamp)
+);
+
+
+CREATE TABLE IF NOT EXISTS silver.quarantine (
+    symbol          TEXT,
+    timestamp       TIMESTAMPTZ,
+    open            DOUBLE PRECISION,
+    high            DOUBLE PRECISION,
+    low             DOUBLE PRECISION,
+    close           DOUBLE PRECISION,
+    volume          DOUBLE PRECISION,
+    trade_count     DOUBLE PRECISION,
+    vwap            DOUBLE PRECISION,
+    ingested_at     TIMESTAMPTZ,
+    source          TEXT,
+    quarantined_at  TIMESTAMPTZ,
+    reason          TEXT
+);
+
+-- Project
+
+CREATE TABLE IF NOT EXISTS silver.regime (
+    symbol        TEXT        NOT NULL,
+    timestamp     TIMESTAMPTZ NOT NULL,
+    open          DOUBLE PRECISION,
+    high          DOUBLE PRECISION,
+    low           DOUBLE PRECISION,
+    close         DOUBLE PRECISION,
+    volume        DOUBLE PRECISION,
+    trade_count   DOUBLE PRECISION,
+    vwap          DOUBLE PRECISION,
+    processed_at   TIMESTAMPTZ NOT NULL DEFAULT now(),
+    PRIMARY KEY (symbol, timestamp)
+);
+
+
+CREATE TABLE IF NOT EXISTS silver.regime_quarantine (
+    symbol          TEXT,
+    timestamp       TIMESTAMPTZ,
+    open            DOUBLE PRECISION,
+    high            DOUBLE PRECISION,
+    low             DOUBLE PRECISION,
+    close           DOUBLE PRECISION,
+    volume          DOUBLE PRECISION,
+    trade_count     DOUBLE PRECISION,
+    vwap            DOUBLE PRECISION,
+    ingested_at     TIMESTAMPTZ,
+    source          TEXT,
+    quarantined_at  TIMESTAMPTZ,
+    reason          TEXT
+);
---
+++ data/sql/test_script.sql
@@ -0,0 +1,23 @@
+-- DROP SCHEMA bronze CASCADE;
+-- DROP SCHEMA silver CASCADE;
+
+SELECT 
+	symbol, "timestamp", "open", high, low, "close", 
+	volume, trade_count, vwap, log_return, hl_range,
+	volume_ratio, rolling_vol, rolling_sharpe, featured_at
+FROM 
+	gold.hw5;
+
+
+select * from bronze.regime
+select * from silver.regime
+
+select count(*) from gold.regime
+select timestamp from gold.regime
+select max(timestamp) from gold.regime
+
+--DELETE FROM bronze.regime 
+--WHERE timestamp = '2026-03-24 04:00:00+00' 
+--AND symbol IN ('QQQ', 'SPY');
+
+select count(*) from bronze.regime r 
